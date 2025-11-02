2c413c8b01c3a25a2accfdbc18e12ed9fe2ad3ae
Paul Williams <paul58914080@gmail.com>
2025-11-02 20:20:32 +0530

    initialize Spring Boot batch application with Maven wrapper and configuration

---
+++ src/main/java/edu/tryoutspringbootbatch/TryoutSpringBootBatchApplication.java
@@ -0,0 +1,13 @@
+package edu.tryoutspringbootbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryoutSpringBootBatchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TryoutSpringBootBatchApplication.class, args);
+  }
+
+}
